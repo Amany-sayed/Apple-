6791f4b93f7aa3acc77a8d2380f4b9569289e239
Loza <120202098+Amany-sayed@users.noreply.github.com>
2025-01-30 21:05:57 +0200

    adding-localStorage-design edited

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-let img =document.querySelector('.img');
-let container =document.querySelector('.container');
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
 
-
-function phones(phone){
-    img.src=phone;
+function phones(phone) {
+    img.src = phone;
+    localStorage.setItem('selectedImg', phone); 
 }
 
-function background(color){
-container.style.background= color;
+
+function background(color) {
+    container.style.background = color;
+    localStorage.setItem('selectedBg', color);
 }
+
+
+window.onload = function () {
+    let savedImg = localStorage.getItem('selectedImg');
+    let savedBg = localStorage.getItem('selectedBg');
+
+    if (savedImg) {
+        img.src = savedImg; 
+    }
+
+    if (savedBg) {
+        container.style.background = savedBg; 
+    }
+};
